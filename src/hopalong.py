7436b340e1784fb77c19b6c0cba5b838b01c1232
Wicmage <victormage@hotmail.com>
2019-12-21 19:58:20 +0100

    Mejor plot

--- src/hopalong.py
+++ src/hopalong.py
@@ -1,29 +1,37 @@
 import sys
 
 import matplotlib.pyplot as mp
+import random as rand
 from numpy import sqrt
 
 class Hopalong:
 
-    def __init__(self, alpha=50.0, beta=50.0, gamma=0.0, random_points=25, iterations=2000):
+    def __init__(self, alpha=50.0, beta=50.0, gamma=0.0, random_points=10, iterations=1000):
         self.alpha = alpha
         self.beta = beta
         self.gamma = gamma
         self.random_points = random_points
         self.iterations = iterations
-        self.x=[0.]
-        self.y=[0.]
+        self.x = list()
+        xx=[rand.random()*100 for _ in range(random_points)]
+        self.x.append(xx)
+        yy=[self.y_function(0,i) for i in range(random_points)]
+        self.y = list()
+        self.y.append(yy)
 
-    def x_function(self, i):
-        return self.y[i]-self.sgn(self.x[i])*sqrt(abs(self.beta*self.x[i]-self.gamma))
+    def x_function(self, i, j):
+        return self.y[i][j]-self.sgn(self.x[i][j])*sqrt(abs(self.beta*self.x[i][j]-self.gamma))
 
-    def y_function(self, i):
-        return self.alpha-self.x[i]
+    def y_function(self, i, j):
+        return self.alpha-self.x[i][j]
 
     def iterar(self):
         for i in range(self.iterations):
-            xx=self.x_function(i)
-            yy=self.y_function(i)
+            xx = []
+            yy = []
+            for j in range(self.random_points):
+                xx.append(self.x_function(i,j))
+                yy.append(self.y_function(i,j))
             self.x.append(xx)
             self.y.append(yy)
 
